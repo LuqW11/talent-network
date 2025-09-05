f6ccb03785104ffb3f76662941e2aa50bc0678c4
LuqW11 <139331586+LuqW11@users.noreply.github.com>
2025-09-05 15:44:55 +0100

    trying to fix deployment issues

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-}
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const config = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default config;
